426d03087e7bfc9e9a9882014485299539bd62a6
Alexei Diaz <alexei.eleusis@gmail.com>
2025-11-12 07:10:43 -0800

    fix: [python:S6711] Use a "numpy.random.Generator" here instead of thi...
    SonarQube Issue: 1d97390c-18d3-4135-87aa-d75394c2a6e6
    Rule: python:S6711 - numpy.random.Generator should be preferred to numpy.random.RandomState
    Severity: MAJOR
    Location: NERF_alexei_eleusis_gmail_com_understanding_251112-1504:run_nerf.py:823
    
    Rationale: https://next.sonarqube.com/sonarqube/coding_rules?open=python:S6711&rule_key=python:S6711
    
    Fixed by: vibe-heal using Claude Code

--- run_nerf.py
+++ run_nerf.py
@@ -820,7 +820,8 @@ def train():
                     coords = torch.stack(torch.meshgrid(torch.linspace(0, H-1, H), torch.linspace(0, W-1, W)), -1)  # (H, W, 2)
 
                 coords = torch.reshape(coords, [-1,2])  # (H * W, 2)
-                select_inds = np.random.choice(coords.shape[0], size=[N_rand], replace=False)  # (N_rand,)
+                rng = np.random.default_rng(0)
+                select_inds = rng.choice(coords.shape[0], size=N_rand, replace=False)  # (N_rand,)
                 select_coords = coords[select_inds].long()  # (N_rand, 2)
                 rays_o = rays_o[select_coords[:, 0], select_coords[:, 1]]  # (N_rand, 3)
                 rays_d = rays_d[select_coords[:, 0], select_coords[:, 1]]  # (N_rand, 3)
